32f5fe69f679675eba049e4555e36def7f01adef
Maciek Sakrejda <m.sakrejda@gmail.com>
2023-03-06 15:02:52 -0800

    Add breakpoints

--- src/constants.js
+++ src/constants.js
@@ -16,3 +16,15 @@ export const WEIGHTS = {
   medium: 600,
   bold: 800,
 };
+
+export const BREAKPOINTS = {
+  phoneMax:	600,
+  tabletMax: 950,
+  laptopMax: 1300,
+}
+
+export const QUERIES = {
+  'phoneAndSmaller': `(min-width: ${BREAKPOINTS.phoneMax / 16}rem)`,
+  'tableAndSmaller': `(min-width: ${BREAKPOINTS.tabletMax / 16}rem)`,
+  'laptopAndSmaller': `(min-width: ${BREAKPOINTS.laptopMax / 16}rem)`,
+}
